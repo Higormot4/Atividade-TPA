539d6aba6532888f00974dbf09fc47c53eef8cab
Higormot4 <166918347+Higormot4@users.noreply.github.com>
2024-09-18 14:49:15 -0300

    Update Pj4.java

--- lacoderepeticao/src/main/java/alg4/Pj4.java
+++ lacoderepeticao/src/main/java/alg4/Pj4.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package alg4;
 
-/**
- *
- * @author FATEC ZONA LESTE
- */
+import java.util.Scanner;
+
+
 public class Pj4 {
-    
+  
+     static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite o valor inicial (ímpar): ");
+    int valorInicial = scanner.nextInt();
+
+    System.out.print("Digite o valor final (ímpar): ");
+    int valorFinal = scanner.nextInt();
+
+    if (valorInicial % 2 == 0) {
+        valorInicial++;
+    }
+    if (valorFinal % 2 == 0) {
+        valorFinal--;
+    }
+
+    int somaImpares = 0;
+
+    for (int numero = valorInicial; numero <= valorFinal; numero += 2) {
+        somaImpares += numero;
+    }
+
+    System.out.println("A soma dos números ímpares entre " + valorInicial + " e " + valorFinal + " é: " + somaImpares);
+
+    scanner.close();
+}
 }
