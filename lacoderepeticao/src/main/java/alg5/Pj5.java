2b7f7d49d1fa3eaa591d9cd0b79c2c23f618d731
Higormot4 <166918347+Higormot4@users.noreply.github.com>
2024-09-18 15:01:41 -0300

    Update Pj5.java

--- lacoderepeticao/src/main/java/alg5/Pj5.java
+++ lacoderepeticao/src/main/java/alg5/Pj5.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package alg5;
+import java.util.Scanner;
 
-/**
- *
- * @author FATEC ZONA LESTE
- */
-public class Pj5 {
-    
+public class NumerosImparesDecrescentes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar ao usuário para digitar os números
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+       
+        if (num1 < num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+       
+        System.out.println("Números ímpares entre " + num1 + " e " + num2 + ":");
+        int i = num1;
+        while (i >= num2) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            
+        }
+
+        scanner.close();
+    }
 }
+
